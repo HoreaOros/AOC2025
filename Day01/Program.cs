3a3bb719e6ef661603e0e792adee51e1a901cbed
Horea Oros <horea.oros@gmail.com>
2025-12-02 09:26:49 +0200

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,70 @@
+﻿#region Input parsing
+string text = File.ReadAllText("input.txt");
+string[] data = text.Split(Environment.NewLine);
+#endregion
+
+#region Part1
+int dial = 50;
+
+
+
+// L99
+char direction;
+int offset;
+int r1 = 0;
+foreach (string s in data)
+{
+    direction = s[0];
+    offset = int.Parse(s.Substring(1));
+    offset = offset % 100;
+    switch (direction)
+    {
+        case 'R':
+            dial = (dial + offset) % 100;
+            break;
+        case 'L':
+            dial = (dial + 100 - offset) % 100;
+            break;
+    }
+    if (dial == 0)
+        r1++;
+}
+
+Console.WriteLine(r1);
+
+
+
+#endregion
+
+#region Part2
+dial = 50;
+
+
+int r2 = 0, r = 0;
+Console.WriteLine($"Dial starts at {dial}");
+foreach (string s in data)
+{
+    direction = s[0];
+    offset = int.Parse(s.Substring(1));
+    r = offset / 100;
+    offset = offset % 100;
+    switch (direction)
+    {
+        case 'R':
+            if(dial + offset >= 100)
+                r++;
+            dial = (dial + offset) % 100;
+            break;
+        case 'L':
+            if (dial != 0 && dial - offset <= 0)
+                r++;
+            dial = (dial + 100 - offset) % 100;
+            break;
+    }
+    r2 += r;
+    //Console.Write($"Rotation {s} - Dial: {dial} - Zero: {r}");
+    //Console.WriteLine();
+}
+
+Console.WriteLine(r2);
+#endregion
---
+++ Day02/Program.cs
@@ -0,0 +1,121 @@
+﻿#region Input parsing
+
+
+string text = File.ReadAllText("input.txt");
+string[] data = text.Split(',');
+List<(long Left, long Right)> intervale = new();
+
+foreach (var item in data)
+{
+    string[] t = item.Split("-");
+    intervale.Add((long.Parse(t[0]), long.Parse(t[1])));
+}
+#endregion
+
+#region Part1
+long r1 = 0;
+foreach(var interval in intervale)
+{
+    for(long nr = interval.Left; nr <= interval.Right; nr++)
+    {
+        //if (InvalidIDStr(nr))
+        //    r1 += nr;
+        if(InvalidIDNum(nr))
+                r1 += nr;
+    }
+}
+
+bool InvalidIDNum(long nr)
+{
+    //int digits = (int)(Math.Abs(Math.Log10(nr))) + 1;
+    int digits = Digits(nr);
+    if (digits % 2 == 1)
+        return false;
+    // 1212
+    long p10 = (long)Math.Pow(10, digits / 2);
+    return nr / p10 == nr % p10;
+}
+
+int Digits(long nr)
+{
+    int cnt = 0;
+    if(nr == 0) return 0;
+    while(nr > 0)
+    {
+        nr /= 10;
+        cnt++;
+    }
+    return cnt;
+}
+
+bool InvalidIDStr(long nr)
+{
+    string num = nr.ToString();
+    if(num.Length % 2 == 1)
+        return false;
+    return num.Substring(0, num.Length / 2) == num.Substring(num.Length / 2);
+}
+
+Console.WriteLine(r1);
+#endregion
+
+#region Part2
+long r2 = 0;
+foreach(var interval in intervale)
+{
+    for (long nr = interval.Left; nr <= interval.Right; nr++)
+    {
+        //if (InvalidIDStr2(nr))
+        //    r2 += nr;
+        if (InvalidIDNum2(nr))
+            r2 += nr;
+    }
+}
+
+bool InvalidIDNum2(long nr)
+{
+    int digits = Digits(nr);
+    long c = nr;
+    for (int len = 1; len <= digits / 2; len++)
+    {
+        nr = c;
+        if (digits % len != 0)
+            continue;
+        List<long> t = new();
+        long p10 = (long)Math.Pow(10, len);
+        while(nr > 0)
+        {
+            t.Add(nr % p10);
+            nr = nr / p10;
+        }
+
+        HashSet<long> s = new HashSet<long>(t);
+        if (s.Count == 1)
+            return true;
+    }
+    return false;
+}
+
+// 464446// 1, 2, 3
+
+
+bool InvalidIDStr2(long nr)
+{
+    string num = nr.ToString();
+    for (int len = 1; len <= num.Length / 2; len++)
+    {
+        if (num.Length % len != 0)
+            continue;
+        List<string> t = new();
+        for(int i = 0; i < num.Length; i += len)
+            t.Add(num.Substring(i, len));
+
+        HashSet<string> s = new HashSet<string>(t);
+        if (s.Count == 1)
+            return true;
+    }
+    return false;
+}
+
+Console.WriteLine(r2);
+#endregion
