eec2dddfee383cfa17499769d917cb1110486f83
Horea Oros <horea.oros@gmail.com>
2025-12-11 09:13:48 +0200

    Add Day11 partial solution

---
+++ Day10/Program.cs
@@ -0,0 +1,211 @@
+﻿#region Input parsing
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+string text = File.ReadAllText("input.txt");
+List<(int V, List<int> Buttons, List<int> Joltages)> machines = new();
+List<(List<List<int>> Buttons, List<int> Joltages)> machines2 = new();
+string[] data = text.Split(Environment.NewLine);
+Regex r = new Regex(@"\[([\.#]+)\](( \((\d+,*)*\))*) \{((\d+,?)+)\}");
+foreach (string s in data)
+{
+    Match result = r.Match(s);
+    string m = result.Groups[1].Value;
+    string b = result.Groups[2].Value;
+    string j = result.Groups[5].Value;
+    int v = 0; // V
+    for (int i = 0; i < m.Length; i++)
+        if (m[i] == '#')
+            v += (1 << i);
+    
+    List<int> lst = new List<int>(); // Buttons
+    List<List<int>> lstP2 = new(); // Buttons for part 2
+    b = b.Trim();
+    string[] t2 = b.Split(' ');
+
+    foreach (string s2 in t2)
+    {
+        string s3 = s2.Trim('(', ')');
+        string[] t3 = s3.Split(',');
+        int value = 0;
+        List<int> lst3 = new List<int>();
+        foreach(var item  in t3)
+        {
+            int exp = int.Parse(item);
+            value += (1 << exp);
+            lst3.Add(exp);
+        }
+        lst.Add(value);
+        lstP2.Add(lst3);
+
+
+    }
+
+    List<int> lst2 = new List<int>(); // joltages
+    string[] t4 = j.Split(',');
+    foreach (string s4 in t4)
+        lst2.Add(int.Parse(s4));
+    machines.Add((v, lst, lst2));
+
+    machines2.Add((lstP2, lst2));
+
+}
+Console.WriteLine();
+#endregion
+
+#region Part1
+int r1 = 0;
+foreach(var machine in machines)
+{
+    int len = machine.Buttons.Count;
+    int max = 1 << len;
+    int minim = int.MaxValue;
+    for(int n = 0; n < max; n++)
+    {
+        int[] pos = new int[len];
+        int c = n;
+        for(int i = 0; i < len; i++)
+        {
+            pos[i] = c % 2;
+            c = c / 2;
+        }
+        int value = 0;
+        for(int i = 0;i < len; i++)
+        {
+            if (pos[i] == 1)
+            {
+                value ^= machine.Buttons[i];
+            }
+        }
+        if(value == machine.V)
+        {
+            if (minim > Ones(n))
+                minim = Ones(n);
+        }
+    }
+    r1 += minim;
+}
+
+int Ones(int n)
+{
+    int r = 0;
+    while (n > 0)
+    {
+        if (n % 2 == 1)
+            r++;
+        n /= 2;
+    }
+    return r;
+}
+
+Console.WriteLine(r1);
+#endregion
+
+#region Part2
+/*int r2 = 0;
+foreach(var machine in machines2)
+{
+    int N = machine.Buttons.Count;
+    int[] MAX = new int[N];
+    for(int i = 0; i < N; i++)
+    {
+        int minim = int.MaxValue;
+        List<int> button = machine.Buttons[i];
+        for (int j = 0; j < button.Count; j++)
+        {
+            if (minim > machine.Joltages[button[j]])
+                minim = machine.Joltages[button[j]];
+        }
+        MAX[i] = minim;
+    }
+
+    r2 += BackT(MAX, machine.Buttons, machine.Joltages);
+    //Console.WriteLine();
+}
+
+int BackT(int[] mAX, List<List<int>> buttons, List<int> joltages)
+{
+    int[] v = new int[buttons.Count];
+    int N = buttons.Count;
+    for (int i = 0; i < N; i++)
+        v[i] = -1;
+    int k = 0;
+    int result = int.MaxValue;
+    bool ams = false, ev = false;
+    while(k >= 0)
+    {
+        do
+        {
+            ams = succesor(v, k, mAX);
+            if (ams)
+                ev = valid(v, k, buttons, joltages);
+        } while (ams && !ev);
+        if (ams)
+        {
+            //for(int i = 0; i <= k; i++)
+            //    Console.Write($"{v[i]}");
+            //Console.WriteLine();
+            if (Solutie(v, k, buttons, joltages))
+            {
+                int pushes = 0;
+                for (int i = 0; i <= k; i++)
+                    pushes += v[i];
+                if (pushes < result)
+                    result = pushes;
+            }
+            else if(k < N - 1)
+            {
+                k++;
+                v[k] = -1;
+            }
+        }
+        else
+            k--;
+    }
+    return result;
+}
+
+bool Solutie(int[] v, int k, List<List<int>> buttons, List<int> joltages)
+{
+    bool result = true;
+    int N = joltages.Count;
+    int[] t = new int[N];
+    for(int i = 0; i <= k; i++)
+    {
+        List<int> lst = buttons[i];
+        for( int j = 0; j < lst.Count; j++)
+        {
+            t[lst[j]] += v[i];
+        }
+    }
+
+
+    for (int i = 0; i < N; i++)
+        if (t[i] != joltages[i])
+            result = false;
+
+    return result;
+}
+
+bool valid(int[] v, int k, List<List<int>> buttons, List<int> joltages)
+{
+    return true;
+}
+
+bool succesor(int[] v, int k, int[] mAX)
+{
+    bool ams = false;
+    if (v[k] < mAX[k])
+    {
+        ams = true;
+        v[k]++;
+    }
+    return ams;
+}
+
+
+Console.WriteLine(r2);
+*/
+
+
+#endregion
---
+++ Day11/Program.cs
@@ -0,0 +1,100 @@
+﻿#region Input parsing
+using System.Text.RegularExpressions;
+
+string text = File.ReadAllText("inputTest.txt");
+string[] lines = text.Split(Environment.NewLine);
+Dictionary<string, List<string>> graph = new();
+//foreach (var line in lines)
+//{
+//    string[] t = line.Split(':');
+//    graph[t[0]] = new List<string>();
+//    string[] t2 = t[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+//    foreach (string s in t2)
+//        graph[t[0]].Add(s);
+
+//}
+
+
+Regex r = new Regex(@"[a-z]{3}");
+foreach (string line in lines)
+{
+    MatchCollection mc = r.Matches(line);
+    List<string> value = new();
+    graph[mc[0].Value] = value;
+    for (int i = 1; i < mc.Count; i++)
+        graph[mc[0].Value].Add(mc[i].Value);
+}
+
+Console.WriteLine();
+StreamWriter sw = new StreamWriter("graph.txt");
+foreach(var item in graph.Keys)
+{
+    foreach(var item2 in graph[item])
+        sw.WriteLine($"{item} {item2}");
+}
+sw.Close();
+#endregion
+
+#region Part1
+string start = "you";
+//Console.WriteLine($"{Part1(start)}");
+
+int Part1(string start)
+{
+    if (start == "out")
+        return 1;
+    int result = 0;
+    foreach (var item in graph[start])
+        result += Part1(item);
+    return result;
+}
+#endregion
+
+#region Part2
+start = "svr";
+Dictionary<string, int> memo = new();
+//Console.WriteLine($"{Part2(start, false, false)}");
+
+List<List<string>> paths = new();
+List<string> path = new();
+Part2v2(start, path);
+Console.WriteLine();
+
+
+
+void Part2v2(string start, List<string> path)
+{
+    path.Add(start);
+    if (start == "out")
+    {
+        paths.Add(path);
+        return;
+    }
+    foreach (var item in graph[start])
+    {
+        Part2v2(item, new List<string>(path));
+    }
+}
+
+int Part2(string start, bool fft, bool dac)
+{
+    if (start == "out")
+        if(fft && dac)
+            return 1;
+        else
+            return 0;
+    if (start == "fft")
+        fft = true;
+    if (start == "dac")
+        dac = true;
+    int result = 0;
+    if (!memo.ContainsKey(start))
+    {
+        foreach (var item in graph[start])
+            result += Part2(item, fft, dac);
+        memo[start] = result; 
+    }    
+
+    return memo[start];
+}
+#endregion
